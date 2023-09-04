16d21f9a341acc941b0e93af5831df182ee79302
fonmusic <red-strat@mail.ru>
2023-09-04 03:27:50 +0300

    Logging added

--- SuperMarket/Program.cs
+++ SuperMarket/Program.cs
@@ -1,6 +1,10 @@
 ﻿using SuperMarket.Classes;
 using SuperMarket.Interfaces;
 
+var logFolderPath = Path.Combine(Directory.GetCurrentDirectory(), "..", "..", "..", "Logs");
+Directory.CreateDirectory(logFolderPath);
+var logFilePath = Path.Combine(logFolderPath, "market_log.txt");
+
 var market = new Market();
 
 IActorBehaviour client1 = new OrdinaryClient("Ivan Petrov");
@@ -12,14 +16,24 @@ IActorBehaviour client6 = new PromotionalClient("Peter", "Beer");
 IActorBehaviour client7 = new SpecialClient("Elizabet", 2);
 
 var numberOfPromotionalParticipants = PromotionalClient.NumberOfPromotionalParticipants;
-Console.WriteLine($"Number of promotional participants: {numberOfPromotionalParticipants}");
+var originalConsoleOut = Console.Out;
+using (var writer = new StreamWriter(logFilePath))
+{
+    Console.SetOut(writer);
 
-market.AcceptToMarket(client1);
-market.AcceptToMarket(client2);
-market.AcceptToMarket(client3);
-market.AcceptToMarket(client4);
-market.AcceptToMarket(client5);
-market.AcceptToMarket(client6);
-market.AcceptToMarket(client7);
+    Console.WriteLine($"Number of promotional participants: {numberOfPromotionalParticipants}");
 
-market.Update();
+    market.AcceptToMarket(client1);
+    market.AcceptToMarket(client2);
+    market.AcceptToMarket(client3);
+    market.AcceptToMarket(client4);
+    market.AcceptToMarket(client5);
+    market.AcceptToMarket(client6);
+    market.AcceptToMarket(client7);
+
+    market.Update();
+    
+    Console.SetOut(originalConsoleOut);
+}
+
+Console.WriteLine(File.ReadAllText(logFilePath));
